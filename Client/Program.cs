1c98b7a1e8edcd8468341d77ec07f8124e8666de
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-06 14:05:03 +0200

    Aggiungere i file di progetto.

---
+++ Client/Program.cs
@@ -0,0 +1,79 @@
+﻿using Libreria;
+/* Il cliente assegna delle attività allo sviluppatore (dal suo file al file dello sviluppatore)
+ * questa attività avviene casualmente in un intervallo 5-10 secondi; ( Thread.Sleep(1000);
+ qualche volta però il cliente viene distratto (il programma si chiude casualmente? - oppure c'è un'eccezione?
+quindi il cliente deve ricominciare possibilmente senza riassegnare task già assegnati allo sviluppatore ( )*/
+
+
+//sviluppi
+
+//1. creare i reader e i writer per leggere dal file ClientActiviti.txt al file DeveloperActivity
+//   quindi apertura in lettura di un file e in scrittura di un altro file.
+
+//2. Leggere la linea = attività e salvarla da qualche parte ---> read su clientActivity.txt
+//3. Scrivere l'attività che abbiamo recuperato nell'altro file ---> write su developerActivity.txt
+
+//4. Tread.Sleep(...tot secondi casuali...)
+
+//5. si ricomincia da punto 2 fino alla fine del file clientActivity.txt (con un ciclo)
+
+StreamReader tasks = File.OpenText("C:/Users/black/source/repos/Live coding pome/csharp-developer-simulator/ClientActivity.txt");
+//StreamWriter developerTasks = new StreamWriter("C:/Users/black/source/repos/Live coding pome/csharp-developer-simulator/DeveloperActivity.txt");
+
+List<string> list = new List<string>();
+List<string> corruptList = new List<string>();
+
+while (!tasks.EndOfStream)
+{
+    try
+    {
+        string task = tasks.ReadLine();
+
+        list.Add(task);
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("File corrotto");
+    }
+}
+
+tasks.Close();
+
+string path = "C:/Users/black/source/repos/Live coding pome/csharp-developer-simulator/DeveloperActivity.txt";
+if(File.Exists(path))
+{
+    foreach(string activity in list)
+    {
+        StreamWriter developerTasks = File.AppendText(path);
+        developerTasks.WriteLine(activity);
+        Console.WriteLine(activity);
+        developerTasks.Close();
+        Thread.Sleep(new Random().Next(3000, 5000));
+    }
+}
+
+
+//Print(list);
+
+//void Print(List<Task> list)
+//{
+//    Console.WriteLine($"\tIndex\tName\t");
+//    foreach (Task task in list)
+//    {
+//        Console.WriteLine($"\t{task.Index}\t{task.Name}\t");
+//    }
+//}
+
+
+//public class Task
+//{
+//    public string Index;
+//    public string Name;
+
+
+//    public Task(string index, string name)
+//    {
+//        Index = index;
+//        Name = name;
+//    }
+//}
---
+++ Developer/Program.cs
@@ -0,0 +1,59 @@
+﻿using Libreria;
+/* lo viluppatore legge le attività del suo file di attività (developerActivity) da risolvere
+ * per risolvere le attività ci impiega dai 3 ai 15 secondi.
+ se le risolve le elimina dalla lista.
+se non le risolve la porta in fondo alla lista per farle in un secondo momento*/
+
+//sviluppi
+
+//1. creare il reader
+//2. aprire il file developerActivity.txt
+//3. leggo la riga facendo dei controlli (perchè può essere che non ci siano ancora task)
+//4. bool risolta = true || false ---> Random().NextDouble() > 0.5;
+//5. risolta == false ---> dobbiamo scrivere il file di modo che l'ultima riga sia il task corrente.
+//5.1risolta == true ---> dobbiamo scrivere il file in modo che la lista sia aggiornata senza il task corrente (eliminarlo).
+//6. ripeto dal punto 
+
+
+void UpdateActivity()
+{
+    string path = "C:/Users/black/source/repos/Live coding pome/csharp-developer-simulator/DeveloperActivity.txt";
+    string[] developerActivity = File.ReadAllLines(path);
+
+    List<string> developerActivityList = developerActivity.ToList();
+
+    if(developerActivityList.Count > 0)
+    {
+        while(developerActivityList.Count > 0)
+        {
+            string activity = developerActivityList[0];
+            if(activity != "")
+            {
+                Console.WriteLine($"presa in carico di {activity}");
+                Thread.Sleep(new Random().Next(2000, 7000));
+
+                if(new Random().NextDouble() > 0.5)
+                {
+                    developerActivityList = File.ReadAllLines(path).ToList();
+                    developerActivityList.Remove(activity);
+                    File.WriteAllLines(path, developerActivityList);
+                    Console.WriteLine("Task completata");
+                }
+                else
+                {
+                    developerActivityList = File.ReadAllLines(path).ToList();
+                    developerActivityList.Remove(activity);
+                    developerActivityList.Add(activity);
+                    File.WriteAllLines(path, developerActivityList);
+                    Console.WriteLine("Task rimandata");
+                }
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Attualmente non ci sono task da svolgere");
+    }
+}
+
+UpdateActivity();
